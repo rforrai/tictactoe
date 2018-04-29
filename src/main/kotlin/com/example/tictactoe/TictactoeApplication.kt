ed7ab8b1a601a4301338370e88817c2fd03d4ffc
Richard Forrai <forrairichard11@gmail.com>
2018-04-29 11:15:10 +0200

    initial commit

---
+++ src/main/kotlin/com/example/tictactoe/TictactoeApplication.kt
@@ -0,0 +1,12 @@
+package com.example.tictactoe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TictactoeApplication
+
+
+fun main(args: Array<String>) {
+    runApplication<TictactoeApplication>(*args)
+}
